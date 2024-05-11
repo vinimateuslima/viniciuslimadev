3203b8f741c70f4fa721620b87bf2ee6c2fa32da
vinimateuslima <viniciusmateus.dev@gmail.com>
2024-05-11 19:47:16 -0300

    ajustando o app

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+const cors = require("cors");
 
 const user = process.env.EMAIL;
 const pass = process.env.SENHA;
@@ -11,10 +12,17 @@ const pass = process.env.SENHA;
 // Middleware para fazer o parse do corpo da requisição como JSON
 app.use(bodyParser.json());
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors());
+
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+  res.setHeader('Access-Control-Allow-Origin', 'https://vinilimadev.com');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST"
+  );
 });
 
 app.post("/send", (req, res) => {
