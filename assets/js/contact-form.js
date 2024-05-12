e8ca440b42887961a7d05404ad8ceb97f50546f1
vinimateuslima <viniciusmateus.dev@gmail.com>
2024-05-11 22:04:52 -0300

    inserindo recaptcha v3 teste2

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -10,53 +10,55 @@ let mensagem = document.getElementById("comments");
 
 const KEY = "6LeKN9kpAAAAADiVFm2hujUe2fgB4toVKR3YD8eL";
 
-function enviarFormulario() {
-  grecaptcha.execute(KEY, { action: "submit" }).then(function (token) {
-    // Insira o token em um campo de formulário oculto
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-	console.log(token);
+  grecaptcha.ready(function () {
+    grecaptcha.execute(KEY, { action: "submit" }).then(function (token) {
+      console.log(token);
 
-    const formData = {
-      email: email.value,
-      assunto: assunto.value,
-      html: `<p>
+      const formData = {
+        email: email.value,
+        assunto: assunto.value,
+        html: `<p>
 	<strong>Nome: </strong>${nome.value} 
 	<br/> 
 	<strong>Telefone: </strong>${telefone.value}
 	<br/> 
 	<strong>Mensagem: </strong>${mensagem.value} </p>`,
-      recaptcha_token: token,
-    };
+        recaptcha_token: token,
+      };
 
-    const options = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formData),
-    };
+      const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      };
 
-    fetch("https://vinilimadev.com/send", options)
-      .then((response) => response.json())
-      .then((data) => {
-        nome.value = "";
-        email.value = "";
-        assunto.value = "";
-        telefone.value = "";
-        mensagem.value = "";
+      fetch("https://vinilimadev.com/send", options)
+        .then((response) => response.json())
+        .then((data) => {
+          nome.value = "";
+          email.value = "";
+          assunto.value = "";
+          telefone.value = "";
+          mensagem.value = "";
 
-        msgSucesso.setAttribute("class", "mensagem");
+          msgSucesso.setAttribute("class", "mensagem");
 
-        setTimeout(() => {
-          msgSucesso.setAttribute("class", "mensagem d-none");
-        }, 3000);
-      })
-      .catch((error) => {
-        console.error("Erro ao enviar mensagem:", error);
+          setTimeout(() => {
+            msgSucesso.setAttribute("class", "mensagem d-none");
+          }, 3000);
+        })
+        .catch((error) => {
+          console.error("Erro ao enviar mensagem:", error);
 
-        msgErro.setAttribute("class", "mensagem");
+          msgErro.setAttribute("class", "mensagem");
 
-        setTimeout(() => {
-          msgErro.setAttribute("class", "mensagem d-none");
-        }, 3000);
-      });
+          setTimeout(() => {
+            msgErro.setAttribute("class", "mensagem d-none");
+          }, 3000);
+        });
+    });
   });
-}
+});
